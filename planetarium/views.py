f1aadec424a7e6509b40aec88b0dcdff5290fec5
alexg <alexgubarev885@gmail.com>
2024-10-17 18:05:45 +0300

    Added base view sets for planetarium models

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from planetarium.models import (
+    ShowTheme,
+    ShowSession,
+    AstronomyShow,
+    Reservation,
+    PlanetariumDome
+)
+from planetarium.serializers import (
+    ShowThemeSerializer,
+    ShowSessionSerializer,
+    AstronomyShowSerializer,
+    ReservationSerializer,
+    PlanetariumDomeSerializer
+)
+
+
+class ShowThemeViewSet(
+    GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
+
+class PlanetariumDomeViewSet(
+    GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
+
+
+class AstronomyShowViewSet(
+    GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin
+):
+    queryset = AstronomyShow.objects.all()
+    serializer_class = AstronomyShowSerializer
+
+
+class ShowSessionViewSet(
+    GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
+    queryset = ShowSession.objects.all()
+    serializer_class = ShowSessionSerializer
+
+
+class ReservationViewSet(
+    GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
