91e3452934d48693f357c8ade219f14375ee3ecf
alexg <alexgubarev885@gmail.com>
2024-10-20 19:29:21 +0300

    Created route for user

--- planetarium_api_service/urls.py
+++ planetarium_api_service/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/planetarium/", include("planetarium.urls", namespace="planetarium"))
+    path("api/planetarium/", include("planetarium.urls", namespace="planetarium")),
+    path("api/user/", include("user.urls", namespace="user"))
 ] + debug_toolbar_urls()
 
