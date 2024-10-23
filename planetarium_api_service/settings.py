be36c74dfba4f85da454650f8a0b6ca63894ebf4
alexg <alexgubarev885@gmail.com>
2024-10-23 19:11:05 +0300

    Added support for Browsable API in DRF settings

--- planetarium_api_service/settings.py
+++ planetarium_api_service/settings.py
@@ -144,7 +144,8 @@ INTERNAL_IPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES":
-        ["rest_framework_simplejwt.authentication.JWTAuthentication"],
+        ["rest_framework_simplejwt.authentication.JWTAuthentication",
+         "rest_framework.authentication.SessionAuthentication"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 7,
     "DEFAULT_PERMISSION_CLASSES":
